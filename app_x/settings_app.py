29839745fb8f4e541d67e8503d2dfd10165ec387
birkin <birkin.diana@gmail.com>
2018-10-09 12:02:58 -0400

    initial commit from template.

---
+++ app_x/settings_app.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+import json, os
+
+
+README_URL = os.environ['DJANGO_TEMPLATE__README_URL']
---
+++ app_x/views.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+import datetime, json, logging, os, pprint
+from . import settings_app
+from app_x.lib import view_info_helper
+from django.conf import settings as project_settings
+from django.contrib.auth import logout
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+
+
+log = logging.getLogger(__name__)
+
+
+def info( request ):
+    """ Returns basic data including branch & commit. """
+    1/0  # tests admin error-emails (remove) -- view output via running, in another terminal window: `python -m smtpd -n -c DebuggingServer localhost:1026`
+    # log.debug( 'request.__dict__, ```%s```' % pprint.pformat(request.__dict__) )
+    rq_now = datetime.datetime.now()
+    commit = view_info_helper.get_commit()
+    branch = view_info_helper.get_branch()
+    info_txt = commit.replace( 'commit', branch )
+    resp_now = datetime.datetime.now()
+    taken = resp_now - rq_now
+    context_dct = view_info_helper.make_context( request, rq_now, info_txt, taken )
+    output = json.dumps( context_dct, sort_keys=True, indent=2 )
+    return HttpResponse( output, content_type='application/json; charset=utf-8' )
