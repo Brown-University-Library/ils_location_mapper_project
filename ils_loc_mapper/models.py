073d3827de0fa096368862bd74a8f50fddb1c3b6
birkin <birkin.diana@gmail.com>
2018-10-12 12:54:18 -0400

    creates model.

--- ils_loc_mapper/models.py
+++ ils_loc_mapper/models.py
@@ -7,3 +7,16 @@ from django.db import models
 from django.http import HttpResponseRedirect
 
 log = logging.getLogger(__name__)
+
+
+class LocationCodeMapper(models.Model):
+    code = models.CharField( max_length=10 )
+    building = models.CharField( max_length=100 )
+    display = models.CharField( max_length=100 )
+    format = models.CharField( max_length=100 )
+    note = models.TextField( blank=True )
+    create_date = models.DateTimeField( auto_now_add=True, help_text='Set automatically on save.' )
+    modify_date = models.DateTimeField( auto_now=True, help_text='Set automatically on save.' )
+
+    def __str__(self):
+        return '%s - %s' % ( self.building, self.format )
