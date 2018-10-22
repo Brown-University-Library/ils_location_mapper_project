54d03b650f0647e21b4d61546df5d158691de34c
birkin <birkin.diana@gmail.com>
2018-10-22 09:09:30 -0400

    adds admin view.

--- ils_loc_mapper/admin.py
+++ ils_loc_mapper/admin.py
@@ -1,3 +1,13 @@
 # -*- coding: utf-8 -*-
 
+from .models import LocationCodeMapper
 from django.contrib import admin
+
+
+class LocationCodeMapperAdmin( admin.ModelAdmin ):
+    list_display = [ 'code', 'building', 'display', 'format', 'note' ]
+    ordering = [ 'code' ]
+    save_on_top = True
+
+
+admin.site.register( LocationCodeMapper, LocationCodeMapperAdmin )
