21480fcb42cc226e0567a0b0db0bc999e8d30c36
birkin <birkin.diana@gmail.com>
2018-10-31 11:47:09 -0400

    adds caching.

--- ils_loc_mapper/lib/mapper_helper.py
+++ ils_loc_mapper/lib/mapper_helper.py
@@ -2,6 +2,7 @@
 
 import datetime, json, logging, pprint
 from . import common
+from django.core.cache import cache
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseNotFound, HttpResponseServerError
 from ils_loc_mapper import settings_app
 from ils_loc_mapper.models import LocationCodeMapper
@@ -40,12 +41,19 @@ class Mapper(object):
         log.debug( 'code_type, `%s`' % code_type )
         return code_type
 
+
+
     def prep_code_data( self, code ):
         """ Performs lookup & returns data.
             Called by views.map_location_code() """
         out = { 'rslt': None, 'err': None }
         try:
-            match = LocationCodeMapper.objects.get( code=code )
+            cache_key = code
+            match = cache.get( cache_key )
+            if match is None:
+                log.debug( 'code-data _not_ from cache' )
+                match = LocationCodeMapper.objects.get( code=code )
+                cache.set( cache_key, match )  # time could be last argument; defaults to settings.py entry
             out['rslt'] = {
                 'building': match.building,
                 'code': match.code,
@@ -58,17 +66,56 @@ class Mapper(object):
         log.debug( 'data-out, ```%s```' % out )
         return out
 
+    # def prep_code_data( self, code ):
+    #     """ Performs lookup & returns data.
+    #         Called by views.map_location_code() """
+    #     out = { 'rslt': None, 'err': None }
+    #     try:
+    #         match = LocationCodeMapper.objects.get( code=code )
+    #         out['rslt'] = {
+    #             'building': match.building,
+    #             'code': match.code,
+    #             'display': match.display,
+    #             'format': match.format
+    #         }
+    #     except Exception as e:
+    #         log.error( 'exception getting data, ```%s```' % e )
+    #         out['err'] = 'not found'
+    #     log.debug( 'data-out, ```%s```' % out )
+    #     return out
+
+
+
     def prep_dump_data( self ):
         """ Returns all data.
             Called by views.map_location_code() """
-        items_dct = {}
-        data_objs = LocationCodeMapper.objects.all().order_by( 'code' )
-        for obj in data_objs:
-            obj_dct = obj.dictify()
-            del( obj_dct['code'] )
-            items_dct[obj.code] = obj_dct
+        cache_key = 'all'
+        items_dct = cache.get( cache_key )
+        if items_dct is None:
+            log.debug( 'dump-data _not_ from cache' )
+            items_dct = {}
+            data_objs = LocationCodeMapper.objects.all().order_by( 'code' )
+            for obj in data_objs:
+                obj_dct = obj.dictify()
+                del( obj_dct['code'] )
+                items_dct[obj.code] = obj_dct
+            cache.set( cache_key, items_dct )  # time could be last argument; defaults to settings.py entry
         log.debug( 'items_dct, ```%s...```' % pprint.pformat(items_dct)[0:100] )
         return items_dct
+
+    # def prep_dump_data( self ):
+    #     """ Returns all data.
+    #         Called by views.map_location_code() """
+    #     items_dct = {}
+    #     data_objs = LocationCodeMapper.objects.all().order_by( 'code' )
+    #     for obj in data_objs:
+    #         obj_dct = obj.dictify()
+    #         del( obj_dct['code'] )
+    #         items_dct[obj.code] = obj_dct
+    #     log.debug( 'items_dct, ```%s...```' % pprint.pformat(items_dct)[0:100] )
+    #     return items_dct
+
+
 
     def prep_code_response( self, data_dct, request, rq_now ):
         """ Returns appropriate response based on data.
